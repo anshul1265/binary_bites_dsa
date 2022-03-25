2f44fa797a4b504f8a08259abdca45589b4b1fea
Anshul <anshulsingla126@gmail.com>
2022-03-25 22:02:25 +0530

    first lecture

---
+++ lec1/1.cpp
@@ -0,0 +1,22 @@
+//
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x[5];
+    cout << &x + 1 << "\n";
+    printf("%x", &x + 1);
+    printf("%d", &x + 1);
+    for (int i = 0; i < 7; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            for (int k = 0; k < 22; k++)
+            {
+            }
+        }
+    }
+
+    return 0;
+}
